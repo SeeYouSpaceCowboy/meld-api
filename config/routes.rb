602054cf353a70660a4c3d17bb1a3800ad007bb0
Mohammed Chisti <93mchisti@gmail.com>
2018-05-05 12:18:20 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ActionCable.server, at: '/cable'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :update, :destroy]
+      get 'user', to: 'users#show', as: 'user_show'
+      post 'signup', to: 'users#create', as: 'user_signup'
+      post 'login', to: 'users#login', as: 'user_login'
+    end
+  end
 end
